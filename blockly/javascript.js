efcd3101e43274a5861654d29226b138038a49db
shengyuan <karokya@gmail.com>
2018-06-26 18:22:45 +0800

    Modify the block, mpu9250_val

--- blockly/javascript.js
+++ blockly/javascript.js
@@ -7,10 +7,7 @@ Blockly.JavaScript['mpu9250_detected'] = function (block) {
   var variable_name_ = Blockly.JavaScript.variableDB_.getName(block.getFieldValue('name_'), Blockly.Variables.NAME_TYPE);
   var dropdown_type_ = block.getFieldValue('type_');
   var statements_do_ = Blockly.JavaScript.statementToCode(block, 'do_');
-  var code = variable_name_ + '.on(' + dropdown_type_ +', async function ( _x, _y, _z) {\n' +
-    '  ' + variable_name_ + '._x = _x;\n' +
-    '  ' + variable_name_ + '._y = _y;\n' +
-    '  ' + variable_name_ + '._z = _z;\n' +
+  var code = variable_name_ + '.on(' + dropdown_type_ +', async function () {\n' +
     statements_do_ +
     '});\n';
   return code;
